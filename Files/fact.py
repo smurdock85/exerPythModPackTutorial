f3dddbb69914530c9d6933ebff225b6696271eb6
smurdock85 <89748811+smurdock85@users.noreply.github.com>
2021-09-11 16:24:19 -0600

    added text

--- Files/fact.py
+++ Files/fact.py
@@ -1,7 +1,8 @@
+#This is the fact module 
 def fact(n):
-    return 1 if n == 1 else n * fact(n-1)
+    return 1 if n == 1 else n * fact(n-1) 
 
 if (__name__ == '__main__'):
-    import sys
+    import sys 
     if len(sys.argv) > 1:
         print(fact(int(sys.argv[1])))
